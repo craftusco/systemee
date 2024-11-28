8dd629e0f18c49ceeb6ed40e110819166e07c449
vitali <v.jalbu31@gmail.com>
2024-11-28 11:32:09 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,42 @@
 <?php
 
-use App\Http\Controllers\ArtistController;
-use App\Http\Controllers\SupplierController;
-use App\Http\Controllers\XDController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+// Auth
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\FacebookController;
+use App\Http\Controllers\Auth\GoogleController;
+
+
+use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\AuthController;
 
 
-Route::get('/', [HomeController::class, 'index'])->name(name: 'index');
+// Auth
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'page'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])->name('forgot-password');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'store']);
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'create'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'store'])->name('password.store');
 
 
-Route::prefix('auth')->name('products')->group(function () {
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/{slug}', [AuthController::class, 'forgotPassword'])->name('forgotPassword');
 });
 
-Route::prefix('products')->name('products')->group(function () {
-    Route::get('/', [ArtistController::class, 'index'])->name('index');
-    Route::get('/{slug}', [ArtistController::class, 'view'])->name('view');
+// User
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name(name: 'index');
+
+    // Artists
+    Route::prefix('artists')->name('artists')->group(function () {
+        Route::get('/', [ArtistController::class, 'index'])->name('index');
+        Route::get('/{slug}', [ArtistController::class, 'view'])->name('view');
+    });
 });
